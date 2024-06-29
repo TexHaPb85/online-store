9baf40db385bec8cecf16b62841343301a2b3ef3
m.nosova <xiatelly@gmail.com>
2024-06-29 19:00:51 +0300

    3: Implemented cart storing infrastructure

---
+++ bussiness-logic-module/src/main/java/edu/hillel/repository/cart/CartRepositoryInMemoryImpl.java
@@ -0,0 +1,63 @@
+package edu.hillel.repository.cart;
+
+import edu.hillel.entities.Cart;
+import org.springframework.stereotype.Repository;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class CartRepositoryInMemoryImpl implements CartRepository {
+    private Map<String, Cart> carts;
+    private static CartRepositoryInMemoryImpl singletonImpl;
+
+    public static synchronized CartRepositoryInMemoryImpl getSingletonInstance(){
+        if (singletonImpl == null){
+            singletonImpl = new CartRepositoryInMemoryImpl();
+        }
+        return singletonImpl;
+    }
+
+    private CartRepositoryInMemoryImpl() {
+        initialize();
+    }
+    private void initialize() {
+        carts = new HashMap<>();
+    }
+
+    private Cart getOrAddCartByUserLogin(String userLogin){
+        if (!carts.containsKey(userLogin)){
+            Cart cart = new Cart();
+            cart.setAddedItems(new HashMap<>());
+            cart.setOwnerOfCart(userLogin);
+            carts.put(userLogin, cart);
+            return cart;
+        }
+        return carts.get(userLogin);
+    }
+
+    @Override
+    public void addItemToCart(String userLogin, Long itemId, Integer numberOfItems) {
+        Cart cart = getOrAddCartByUserLogin(userLogin);
+        cart.addItemToCart(itemId, numberOfItems);
+    }
+
+    @Override
+    public Map<Long, Integer> getAddedItems(String userLogin) {
+        Cart cart = getOrAddCartByUserLogin(userLogin);
+        return Collections.unmodifiableMap(cart.getAddedItems());
+    }
+
+    @Override
+    public void removeAllItemsById(String userLogin, Long id) {
+        Cart cart = getOrAddCartByUserLogin(userLogin);
+        cart.removeAllItemsById(id);
+    }
+
+    @Override
+    public void updateItemsCountById(String userLogin, Long id, Integer numberOfItems) {
+        Cart cart = getOrAddCartByUserLogin(userLogin);
+        cart.updateItemsCountById(id, numberOfItems);
+    }
+}
+
--- online-store-console-service/src/main/java/edu/hillel/App.java
+++ online-store-console-service/src/main/java/edu/hillel/App.java
@@ -2,6 +2,9 @@ package edu.hillel;
 
 import java.util.Scanner;
 
+import edu.hillel.repository.cart.CartRepository;
+import edu.hillel.repository.cart.CartRepositoryInMemoryImpl;
+import edu.hillel.repository.cart.CartRepositoryInTxtImpl;
 import edu.hillel.repository.item.ItemRepository;
 import edu.hillel.repository.item.ItemRepositoryInMemoryImpl;
 import edu.hillel.repository.user.UserRepository;
@@ -16,7 +19,8 @@ public class App {
     static UserService userService = new UserService(userRepository);
     static ItemRepository itemRepository = ItemRepositoryInMemoryImpl.getSingeltonInstance();
     static ItemService itemService = ItemService.getSingletonInstance(itemRepository);
-    static CartService cartService = new CartService(itemRepository);
+    static CartRepository cartRepository = CartRepositoryInTxtImpl.getSingletonInstance();
+    static CartService cartService = new CartService(itemRepository, cartRepository);
 
     public static void main(String[] args) {
 
@@ -29,20 +33,29 @@ public class App {
                 userService.logIn(email, password);
             } else {
                 System.out.println(
-                    "Choose option:\n" +
-                        "your cart: " + cartService.getCartContent() + " total amount: " + cartService.getTotalCartAmount());
+                    "[Choose option]\n" +
+                    "Your cart:\n" + cartService.getCartContent() +
+                    "Total amount: " + cartService.getTotalCartAmount());
                 System.out.println(
                     "1. add item to the cart\n" +
-                        "2. make order for selected items\n" +
-                        "3. log out");
+                    "2. remove all items by ID from the cart\n" +
+                    "3. update count of items by ID\n" +
+                    "4. make order for selected items\n" +
+                    "5. log out");
                 int option = scanner.nextInt();
                 switch (option) {
                     case 1:
                         addItemToCart();
                         break;
                     case 2:
+                        removeAllItemsByIdFromCart();
                         break;
                     case 3:
+                        updateItemsCountById();
+                        break;
+                    case 4:
+                        break;
+                    case 5:
                         userService.logOut();
                         break;
                 }
@@ -58,5 +71,19 @@ public class App {
         System.out.println("Enter how much do you want to buy");
         Integer numberOfItems = scanner.nextInt();
         cartService.addItemToCart(id, numberOfItems);
+    }
+
+    private static void removeAllItemsByIdFromCart(){
+        System.out.println("Enter id of items you want to remove:");
+        Long id = scanner.nextLong();
+        cartService.removeAllItemsById(id);
+    }
+
+    private static void updateItemsCountById(){
+        System.out.println("Enter id of items you want to change:");
+        Long id = scanner.nextLong();
+        System.out.println("Enter new count of item:");
+        int numberOfItems = scanner.nextInt();
+        cartService.updateItemsCountById(id, numberOfItems);
     }
 }
