3daef41cbdea20bf9eab9531c95e62ac8881f543
v.starchevskyi <v.starchevskyi@zakaz.global>
2024-07-27 11:19:13 +0300

    draft commit

---
+++ online-store-jsp-web-service/src/main/java/edu/hillel/controller/LoginServlet.java
@@ -0,0 +1,13 @@
+package edu.hillel.controller;
+
+import edu.hillel.repository.user.UserRepository;
+import edu.hillel.service.UserService;
+
+import javax.servlet.http.HttpServlet;
+
+public class LoginServlet extends HttpServlet {
+    private UserRepository userRepository;
+    private UserService userService;
+
+
+}
